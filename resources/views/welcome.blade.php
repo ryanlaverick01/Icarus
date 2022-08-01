f151fde7cb2822ecb0dbb56da55509899689b93b
ryanlaverick01 <87074011+ryanlaverick01@users.noreply.github.com>
2022-08-01 14:07:17 +0100

    fix title

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -4,7 +4,7 @@
         <meta charset="utf-8">
         <meta name="viewport" content="width=device-width, initial-scale=1">
 
-        <title>Transaction Error Review</title>
+        <title>Icarus</title>
         <link rel="stylesheet" type="text/css" href="../css/app.css">
         <link href="https://fonts.googleapis.com/css2?family=Montserrat:wght@100;200;300;400;500;600;700;800;900&family=Roboto&display=swap" rel="stylesheet">
     </head>
