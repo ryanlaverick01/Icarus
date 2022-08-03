607a9bb317e7b61eca60c4607023fef9cd8df532
ryanlaverick01 <87074011+ryanlaverick01@users.noreply.github.com>
2022-08-03 14:31:13 +0100

    create exception to be thrown when query results are empty

---
+++ app/Exceptions/NoHolidaysFoundFromQueryException.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace App\Exceptions;
+
+use Exception;
+
+class NoHolidaysFoundFromQueryException extends Exception
+{
+    public function __construct()
+    {
+        parent::__construct();
+    }
+
+    public function render()
+    {
+        return response()->json([
+            'status' => 404,
+            'error' => 'No Holidays Found'
+        ]);
+    }
+}
