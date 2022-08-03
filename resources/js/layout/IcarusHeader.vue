d459906d4b8b2ee980fca58f7bc7c717a92e3e8a
ryanlaverick01 <87074011+ryanlaverick01@users.noreply.github.com>
2022-08-03 14:34:09 +0100

    create header component

---
+++ resources/js/layout/IcarusHeader.vue
@@ -0,0 +1,13 @@
+<template>
+    <header class="h-16 w-full bg-slate-800 drop-shadow-md z-30"></header>
+</template>
+
+<script>
+export default {
+    name: "IcarusHeader"
+}
+</script>
+
+<style scoped>
+
+</style>
