d1882f288adaed2430cc32aa3e9b582207d0a909
ryanlaverick01 <87074011+ryanlaverick01@users.noreply.github.com>
2022-08-04 14:18:59 +0100

    move to chat component

--- resources/js/views/Dashboard.vue
+++ resources/js/views/Dashboard.vue
@@ -3,11 +3,5 @@
 </template>
 
 <script setup>
-import useChat from "../useChat";
 import Chat from "../components/Chat";
-const { addIcarusMessage } = useChat();
-
-addIcarusMessage("Hey there, I'm Icarus - your friendly Holiday Chatbot!");
-addIcarusMessage("Start chatting to find some holidays I think you'd be interested in!", 3000);
-addIcarusMessage("Okay... you're looking for a holiday - do you want to be somewhere MILD, HOT, or COLD?", 4500);
 </script>
