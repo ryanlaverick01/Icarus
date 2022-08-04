0471325a873526181a6d16d0bfd4ad5e4eab9008
ryanlaverick01 <87074011+ryanlaverick01@users.noreply.github.com>
2022-08-04 14:20:07 +0100

    remove export

--- resources/js/layout/IcarusHeader.vue
+++ resources/js/layout/IcarusHeader.vue
@@ -2,12 +2,4 @@
     <header class="h-16 w-full bg-slate-800 drop-shadow-md z-30"></header>
 </template>
 
-<script>
-export default {
-    name: "IcarusHeader"
-}
-</script>
-
-<style scoped>
-
-</style>
+<script setup></script>
