d75ddc6c517633cca2e9c895fc08662c43c9f6b1
ryanlaverick01 <87074011+ryanlaverick01@users.noreply.github.com>
2022-08-05 15:23:11 +0100

    add final missing comments

--- resources/js/app.js
+++ resources/js/app.js
@@ -2,5 +2,7 @@ import { createApp } from "vue";
 import App from "./App.vue";
 import router from "./router";
 
+//Create application using "App" as base component.
 const app = createApp(App);
+//Tell Vue to use imported router, mount the app to the "#app" container located in @/resources/views/welcome.blade.php
 app.use(router).mount("#app");
